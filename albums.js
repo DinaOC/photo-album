c2bd43c7c99442758ca16c28f67cf324ffc79c2e
Dina O'Connor <DinaOC@Dinas-MacBook-Pro.local>
2016-02-22 14:54:50 -0500

    each album shows pictures

--- albums.js
+++ albums.js
@@ -1,197 +1,121 @@
-var albums = [
-{
-  album1:{
-type: "photoAlbums",
-albumTitle: "Tropics",
-albumUrl:"images/Album1-pic1-thumb.jpg",
-navButtonOne: "button-1",
-},
-
-pic_one_al1: {
-
-  photoName:"Photo-one",
-    photoUrl:"images/Album1-pic2.jpeg",
-    photoThumbUrl:"images/Album1-pic2-thumb.jpg",
-
-},
-
-pic_two_al1:  {
-
-    photoName:"Photo-two",
-    photoUrl:"images/Album1-pic3.jpeg",
-    photoThumbUrl:"images/Album1-pic3-thumb.jpg",
-
-},
-
-pic_three_al1: {
-
-  photoName:"Photo-three",
-  photoUrl:"images/Album1-pic4.jpeg",
-  photoThumbUrl:"images/Album1-pic4-thumb.jpg",
-
-},
-// THIS IS SECOND ALBUM!
-
-album2:{
-type: "photoAlbums",
-albumTitle: "Tech",
-albumUrl:"images/Album2-pic1-thumb.jpg",
-navButtonTwo: "button-2",
-},
-
-pic_one_al2: {
-
-photoName:"Photo-one",
-  photoUrl:"images/Album2-pic2.jpeg",
-  photoThumbUrl:"images/Album2-pic2-thumb.jpg",
-
-},
-
-pic_two_al2:  {
-
-  photoName:"Photo-two",
-  photoUrl:"images/Album2-pic3.jpeg",
-  photoThumbUrl:"images/Album2-pic3-thumb.jpg",
-
-},
-
-pic_three_al2: {
-
-photoName:"Photo-three",
-photoUrl:"images/Album2-pic4.jpeg",
-photoThumbUrl:"images/Album2-pic4-thumb.jpg",
-
-},
-// THIS IS THIRD ALBUM!!!
-
-album3:{
-type: "photoAlbums",
-albumTitle: "City Life",
-albumUrl:"images/Album3-pic1-thumb.jpg",
-navButtonThree: "button-3",
-},
-
-pic_one_al3: {
-
-photoName:"Photo-one",
-  photoUrl:"images/Album3-pic2.jpeg",
-  photoThumbUrl:"images/Album3-pic2-thumb.jpg",
-
-},
-
-pic_two_al3:  {
-
-  photoName:"Photo-two",
-  photoUrl:"images/Album3-pic3.jpeg",
-  photoThumbUrl:"images/Album3-pic3-thumb.jpeg",
-
-},
-
-pic_three_al3: {
-
-photoName:"Photo-three",
-photoUrl:"images/Album3-pic4.jpeg",
-photoThumbUrl:"images/Album3-pic4-thumb.jpg",
-
-},
-
-// THIS IS FOURTH ALBUM
-
-album4:{
-type: "photoAlbums",
-albumTitle: "Vegan",
-albumUrl:"images/Album4-pic1-thumb.jpg",
-navButtonFour: "button-4",
-},
-
-pic_one_al4: {
-
-photoName:"Photo-one",
-  photoUrl:"images/Album4-pic2.jpeg",
-  photoThumbUrl:"images/Album4-pic2-thumb.jpg",
-
-},
-
-pic_two_al4:  {
-
-  photoName:"Photo-two",
-  photoUrl:"images/Album4-pic3.jpeg",
-  photoThumbUrl:"images/Album4-pic3-thumb.jpg",
-
-},
-
-pic_three_al4: {
-
-photoName:"Photo-three",
-photoUrl:"images/Album4-pic4.jpeg",
-photoThumbUrl:"images/Album4-pic4-thumb.jpg",
-
-},
-// THIS IS FIFTH ALBUM!!!
-
-album5:{
-type: "photoAlbums",
-albumTitle: "Wild Life",
-albumUrl:"images/Album5-pic1-thumb.jpg",
-navButtonFive: "button-5",
-},
-
-pic_one_al5: {
-
-photoName:"Photo-one",
-  photoUrl:"images/Album5-pic2.jpeg",
-  photoThumbUrl:"images/Album5-pic2-thumb.jpg",
-
-},
-
-pic_two_al5:  {
-
-  photoName:"Photo-two",
-  photoUrl:"images/Album5-pic3.jpeg",
-  photoThumbUrl:"images/Album5-pic3-thumb.jpg",
-
-},
-
-pic_three_al5: {
-
-photoName:"Photo-three",
-photoUrl:"images/Album5-pic4.jpeg",
-photoThumbUrl:"images/Album5-pic4-thumb.jpg",
-
-},
-// THIS IS SIXTH ALBUM!!!
-
-album6:{
-type: "photoAlbums",
-albumTitle: "Views",
-albumUrl:"images/Album6-pic1-thumb.jpg",
-navButtonSix: "button-6",
-},
-
-pic_one_al6: {
-
-photoName:"Photo-one",
-  photoUrl:"images/Album6-pic2.jpeg",
-  photoThumbUrl:"images/Album6-pic2-thumb.jpg",
-
-},
-
-pic_two_al6:  {
-
-  photoName:"Photo-two",
-  photoUrl:"images/Album6-pic3.jpeg",
-  photoThumbUrl:"images/Album6-pic3-thumb.jpg",
-
-},
-
-pic_three_al6: {
-
-photoName:"Photo-three",
-photoUrl:"images/Album6-pic4.jpeg",
-photoThumbUrl:"images/Album6-pic4-thumb.jpg",
-
+var albums = {
+    album1: {
+        type: "photoAlbums",
+        albumTitle: "Tropics",
+        albumUrl: "images/Album1-pic1-thumb.jpg",
+        navButtonOne: "Album-one",
+        photos: [{
+            photoName: "Photo-one",
+            photoUrl: "images/Album1-pic2.jpeg",
+            photoThumbUrl: "images/Album1-pic2-thumb.jpg",
+        }, {
+            photoName: "Photo-two",
+            photoUrl: "images/Album1-pic3.jpeg",
+            photoThumbUrl: "images/Album1-pic3-thumb.jpg",
+        }, {
+            photoName: "Photo-three",
+            photoUrl: "images/Album1-pic4.jpeg",
+            photoThumbUrl: "images/Album1-pic4-thumb.jpg",
+        }],
+    },
+    // THIS IS SECOND ALBUM!
+    album2: {
+        type: "photoAlbums",
+        albumTitle: "Tech",
+        albumUrl: "images/Album2-pic1-thumb.jpg",
+        navButtonTwo: "Album-two",
+        photos: [{
+            photoName: "Photo-one",
+            photoUrl: "images/Album2-pic2.jpeg",
+            photoThumbUrl: "images/Album2-pic2-thumb.jpg",
+        }, {
+            photoName: "Photo-two",
+            photoUrl: "images/Album2-pic3.jpeg",
+            photoThumbUrl: "images/Album2-pic3-thumb.jpg",
+        }, {
+            photoName: "Photo-three",
+            photoUrl: "images/Album2-pic4.jpeg",
+            photoThumbUrl: "images/Album2-pic4-thumb.jpg",
+        }],
+    },
+    // THIS IS THIRD ALBUM!!!
+    album3: {
+        type: "photoAlbums",
+        albumTitle: "City Life",
+        albumUrl: "images/Album3-pic1-thumb.jpg",
+        navButtonThree: "Album-three",
+        photos: [{
+            photoName: "Photo-one",
+            photoUrl: "images/Album3-pic2.jpeg",
+            photoThumbUrl: "images/Album3-pic2-thumb.jpg",
+        }, {
+            photoName: "Photo-two",
+            photoUrl: "images/Album3-pic3.jpeg",
+            photoThumbUrl: "images/Album3-pic3-thumb.jpeg",
+        }, {
+            photoName: "Photo-three",
+            photoUrl: "images/Album3-pic4.jpeg",
+            photoThumbUrl: "images/Album3-pic4-thumb.jpg",
+        }],
+    },
+    // THIS IS FOURTH ALBUM
+    album4: {
+        type: "photoAlbums",
+        albumTitle: "Vegan",
+        albumUrl: "images/Album4-pic1-thumb.jpg",
+        navButtonFour: "Album-four",
+        photos: [{
+            photoName: "Photo-one",
+            photoUrl: "images/Album4-pic2.jpeg",
+            photoThumbUrl: "images/Album4-pic2-thumb.jpg",
+        }, {
+            photoName: "Photo-two",
+            photoUrl: "images/Album4-pic3.jpeg",
+            photoThumbUrl: "images/Album4-pic3-thumb.jpg",
+        }, {
+            photoName: "Photo-three",
+            photoUrl: "images/Album4-pic4.jpeg",
+            photoThumbUrl: "images/Album4-pic4-thumb.jpg",
+        }],
+    },
+    // THIS IS FIFTH ALBUM!!!
+    album5: {
+        type: "photoAlbums",
+        albumTitle: "Wild Life",
+        albumUrl: "images/Album5-pic1-thumb.jpg",
+        navButtonFive: "Album-five",
+        photos: [{
+            photoName: "Photo-one",
+            photoUrl: "images/Album5-pic2.jpeg",
+            photoThumbUrl: "images/Album5-pic2-thumb.jpg",
+        }, {
+            photoName: "Photo-two",
+            photoUrl: "images/Album5-pic3.jpeg",
+            photoThumbUrl: "images/Album5-pic3-thumb.jpg",
+        }, {
+            photoName: "Photo-three",
+            photoUrl: "images/Album5-pic4.jpeg",
+            photoThumbUrl: "images/Album5-pic4-thumb.jpg",
+        }],
+    },
+    // THIS IS SIXTH ALBUM!!!
+    album6: {
+        type: "photoAlbums",
+        albumTitle: "Views",
+        albumUrl: "images/Album6-pic1-thumb.jpg",
+        navButtonSix: "Album-six",
+        photos: [{
+            photoName: "Photo-one",
+            photoUrl: "images/Album6-pic2.jpeg",
+            photoThumbUrl: "images/Album6-pic2-thumb.jpg",
+        }, {
+            photoName: "Photo-two",
+            photoUrl: "images/Album6-pic3.jpeg",
+            photoThumbUrl: "images/Album6-pic3-thumb.jpg",
+        }, {
+            photoName: "Photo-three",
+            photoUrl: "images/Album6-pic4.jpeg",
+            photoThumbUrl: "images/Album6-pic4-thumb.jpg",
+        }],
+    }
 }
-
-
-}
-]
--- app.js
+++ app.js
@@ -6,214 +6,66 @@ $('.nav-bar').hide();
 $('.first-page').on('click','img', function(event) {
   event.preventDefault();
   var selectedItem = $(this).attr('rel');
-  var albumToPutOnPage =albums [0][selectedItem];
-  $(selectedItem).siblings().hide();
-  $(albumToPutOnPage).show();
+  var onPage = albums[selectedItem];
+  var albumStr ="";
+  onPage.photos.forEach(function(el) {
+    albumStr +=  "<li>"
+             + "<img src='"
+             + el.photoThumbUrl
+             + "'  rel = '" + el.photoName + "' />"
+             +"</li>"
+  })
+  $('.first-page').html(albumStr);
+})
+
+
+
 })
 
 var photoStr ="";
-albums.forEach(function(element){
+
 
  photoStr +=  "<li>"
 + "<img src='"
-+ element.album1.albumUrl
++ albums.album1.albumUrl
 + "'  rel = 'album1' />"
 +"</li>"
-//+ "<h4>"
-//+ element.album1.albumTitle
-//+ "</h4>"
 + "<li>"
 + "<img src='"
-+ element.album2.albumUrl
++ albums.album2.albumUrl
 + "' rel = 'album2' />"
 +"</li>"
-//+ "<h4>"
-//+ element.album2.albumTitle
-//+ "</h4>"
 +"<li>"
 + "<img src='"
-+ element.album3.albumUrl
++ albums.album3.albumUrl
 + "' rel = 'album3' />"
 +"</li>"
-//+ "<h4>"
-//+ element.album3.albumTitle
-//+ "</h4>"
 +"<li>"
 +"<img src='"
-+ element.album4.albumUrl
++ albums.album4.albumUrl
 + "' rel = 'album4' />"
 +"</li>"
-//+ "<h4>"
-//+ element.album4.albumTitle
 + "</h4>"
 +"<li>"
 + "<img src='"
-+ element.album5.albumUrl
++ albums.album5.albumUrl
 + "' rel = 'album5' />"
 +"</li>"
-//+ "<h4>"
-//+ element.album5.albumTitle
-//+ "</h4>"
 +"<li>"
 + "<img src='"
-+ element.album6.albumUrl
++ albums.album6.albumUrl
 + "' rel = 'album6' />"
 +"</li>"
-//+ "<h4>"
-//+ element.album6.albumTitle
-//+ "</h4>"
 
-
-})
-console.log(photoStr);
 $(".first-page").append(photoStr);
 
-// THIS IS ALBUM ONE
-var inAlbumOne = "";
-albums.forEach(function(el){
 
-inAlbumOne += "<li>"
-+ "<img src='"
-+ el.pic_one_al1.photoThumbUrl + "' />"
-+ "</li>"
-+ "<li>"
-+ "<img src='"
-+ el.pic_two_al1.photoThumbUrl + "' />"
-+ "</li>"
-+ "<li>"
-+ "<img src='"
-+ el.pic_three_al1.photoThumbUrl + "' />"
-+ "</li>"
+//thingWeClickedOn.attr('src').replace('-thumb.jpg','.jpeg')
 
-
-})
-
-console.log(inAlbumOne);
-$(".second-page").append(inAlbumOne);
-//THIS IS ALBUM TWO
-
-var inAlbumTwo = "";
-albums.forEach(function(el){
-
-inAlbumTwo += "<li>"
-+ "<img src='"
-+ el.pic_one_al2.photoThumbUrl + "' />"
-+ "</li>"
-+ "<li>"
-+ "<img src='"
-+ el.pic_two_al2.photoThumbUrl + "' />"
-+ "</li>"
-+ "<li>"
-+ "<img src='"
-+ el.pic_three_al2.photoThumbUrl + "' />"
-+ "</li>"
-
-
-})
-
-console.log(inAlbumTwo);
-$(".second-page").append(inAlbumTwo);
-
-// THIS IS ALBUM THREE
-
-var inAlbumThree = "";
-albums.forEach(function(el){
-
-inAlbumThree += "<li>"
-+ "<img src='"
-+ el.pic_one_al3.photoThumbUrl + "' rel = 'album3' />"
-+ "</li>"
-+ "<li>"
-+ "<img src='"
-+ el.pic_two_al3.photoThumbUrl + "' />"
-+ "</li>"
-+ "<li>"
-+ "<img src='"
-+ el.pic_three_al3.photoThumbUrl + "' />"
-+ "</li>"
-
-
-})
-
-console.log(inAlbumThree);
-$(".second-page").append(inAlbumThree);
-
-//THIS IS ALBUM FOUR
-
-var inAlbumFour = "";
-albums.forEach(function(el){
-
-inAlbumFour += "<li>"
-+ "<img src='"
-+ el.pic_one_al4.photoThumbUrl + "' />"
-+ "</li>"
-+ "<li>"
-+ "<img src='"
-+ el.pic_two_al4.photoThumbUrl + "' />"
-+ "</li>"
-+ "<li>"
-+ "<img src='"
-+ el.pic_three_al4.photoThumbUrl + "' />"
-+ "</li>"
-
-
-})
-
-console.log(inAlbumFour);
-$(".second-page").append(inAlbumFour);
-
-// THIS IS ALBUM FIVE
-
-var inAlbumFive = "";
-albums.forEach(function(el){
-
-inAlbumFive += "<li>"
-+ "<img src='"
-+ el.pic_one_al5.photoThumbUrl + "' />"
-+ "</li>"
-+ "<li>"
-+ "<img src='"
-+ el.pic_two_al5.photoThumbUrl + "' />"
-+ "</li>"
-+ "<li>"
-+ "<img src='"
-+ el.pic_three_al5.photoThumbUrl + "' />"
-+ "</li>"
-
-
-})
-
-console.log(inAlbumFive);
-$(".second-page").append(inAlbumFive);
-
-
-// THIS IS ALBUM SIX
-
-var inAlbumSix = "";
-albums.forEach(function(el){
-
-inAlbumSix += "<li>"
-+ "<img src='"
-+ el.pic_one_al6.photoThumbUrl + "' />"
-+ "</li>"
-+ "<li>"
-+ "<img src='"
-+ el.pic_two_al6.photoThumbUrl + "' />"
-+ "</li>"
-+ "<li>"
-+ "<img src='"
-+ el.pic_three_al6.photoThumbUrl + "' />"
-+ "</li>"
-
-
-})
-
-console.log(inAlbumSix);
-$(".second-page").append(inAlbumSix);
 
 // THESE ARE SIDE BUTTONS
 
-var albumBtn = "";
+/*var albumBtn = "";
 albums.forEach(function(elButn){
 
   albumBtn += "<button>"+ elButn.navButtonOne + "</button"
@@ -223,11 +75,145 @@ albums.forEach(function(elButn){
   +"<button>"+ elButn.navButtonFive + "</button"
   +"<button>"+ elButn.navButtonSix + "</button"
 
-  console.log(albumBtn);
   $(".nav-bar").append(albumBtn);
 
 
-})
+})*/
 
 
-})
+// THIS IS ALBUM ONE
+// var inAlbumOne = "";
+// albums.forEach(function(el){
+//
+// inAlbumOne += "<li>"
+// + "<img src='"
+// + el.pic_one_al1.photoThumbUrl + "' />"
+// + "</li>"
+// + "<li>"
+// + "<img src='"
+// + el.pic_two_al1.photoThumbUrl + "' />"
+// + "</li>"
+// + "<li>"
+// + "<img src='"
+// + el.pic_three_al1.photoThumbUrl + "' />"
+// + "</li>"
+//
+//
+// })
+//
+// console.log(inAlbumOne);
+// $(".second-page").append(inAlbumOne);
+// //THIS IS ALBUM TWO
+//
+// var inAlbumTwo = "";
+// albums.forEach(function(el){
+//
+// inAlbumTwo += "<li>"
+// + "<img src='"
+// + el.pic_one_al2.photoThumbUrl + "' />"
+// + "</li>"
+// + "<li>"
+// + "<img src='"
+// + el.pic_two_al2.photoThumbUrl + "' />"
+// + "</li>"
+// + "<li>"
+// + "<img src='"
+// + el.pic_three_al2.photoThumbUrl + "' />"
+// + "</li>"
+//
+//
+// })
+//
+// $(".second-page").append(inAlbumTwo);
+//
+// // THIS IS ALBUM THREE
+//
+// var inAlbumThree = "";
+// albums.forEach(function(el){
+//
+// inAlbumThree += "<li>"
+// + "<img src='"
+// + el.pic_one_al3.photoThumbUrl + "' rel = 'album3' />"
+// + "</li>"
+// + "<li>"
+// + "<img src='"
+// + el.pic_two_al3.photoThumbUrl + "' />"
+// + "</li>"
+// + "<li>"
+// + "<img src='"
+// + el.pic_three_al3.photoThumbUrl + "' />"
+// + "</li>"
+//
+//
+// })
+//
+// $(".second-page").append(inAlbumThree);
+//
+// //THIS IS ALBUM FOUR
+//
+// var inAlbumFour = "";
+// albums.forEach(function(el){
+//
+// inAlbumFour += "<li>"
+// + "<img src='"
+// + el.pic_one_al4.photoThumbUrl + "' />"
+// + "</li>"
+// + "<li>"
+// + "<img src='"
+// + el.pic_two_al4.photoThumbUrl + "' />"
+// + "</li>"
+// + "<li>"
+// + "<img src='"
+// + el.pic_three_al4.photoThumbUrl + "' />"
+// + "</li>"
+//
+//
+// })
+//
+// $(".second-page").append(inAlbumFour);
+//
+// // THIS IS ALBUM FIVE
+//
+// var inAlbumFive = "";
+// albums.forEach(function(el){
+//
+// inAlbumFive += "<li>"
+// + "<img src='"
+// + el.pic_one_al5.photoThumbUrl + "' />"
+// + "</li>"
+// + "<li>"
+// + "<img src='"
+// + el.pic_two_al5.photoThumbUrl + "' />"
+// + "</li>"
+// + "<li>"
+// + "<img src='"
+// + el.pic_three_al5.photoThumbUrl + "' />"
+// + "</li>"
+//
+//
+// })
+//
+// $(".second-page").append(inAlbumFive);
+//
+//
+// // THIS IS ALBUM SIX
+//
+// var inAlbumSix = "";
+// albums.forEach(function(el){
+//
+// inAlbumSix += "<li>"
+// + "<img src='"
+// + el.pic_one_al6.photoThumbUrl + "' />"
+// + "</li>"
+// + "<li>"
+// + "<img src='"
+// + el.pic_two_al6.photoThumbUrl + "' />"
+// + "</li>"
+// + "<li>"
+// + "<img src='"
+// + el.pic_three_al6.photoThumbUrl + "' />"
+// + "</li>"
+//
+//
+// });
+// $(".second-page").append(inAlbumSix);
